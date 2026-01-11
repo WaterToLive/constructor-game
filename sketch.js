13f21c83a0accfa45c67d25c0d421163b886fb61
WaterToLive <bilalftoure@gmail.com>
2026-01-11 19:17:32 +0000

    Changed from windowheight to just Height

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let die= [];
 function setup () {
   createCanvas(windowWidth,windowHeight); 
   for(let i =0; i < 12; i++) {
-    die.push(new FallingSquare(random(width), 0 , random(20,35), "blue"));
+    die.push(new FallingSquare(random(width), 0 , random(50,75), "blue"));
   }
 } 
 
