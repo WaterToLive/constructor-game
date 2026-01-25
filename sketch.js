1692bcff8121689bfe2f181b529c4a2bf053f0f0
WaterToLive <bilalftoure@gmail.com>
2026-01-25 19:49:41 +0000

    Changed speed

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ function setup () {
   createCanvas(windowWidth,windowHeight); 
   for(let i =0; i < 12; i++) {
     die.push(new FallingSquare(random(width), 0 , random(50,75), "blue"));
-    die.push(new FallingCircle(random(width), 0 , random(50,75), "yellow"));
-    die.push(new FallingTriangle(random(width), 0, random(50,75),"orange"));
+    die.push(new FallingCircle(random(width), 0 , random(25,45), "yellow"));
+    die.push(new FallingTriangle(random(width), 0, random(9,12),"orange"));
   }
 } 
 
