9d8eaf3c5e1c6f9fe51062eb15b35e2d1c29598e
WaterToLive <bilalftoure@gmail.com>
2026-02-08 19:56:22 +0000

    size adjusted

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ function setup () {
   createCanvas(windowWidth,windowHeight); 
   for(let i =0; i < 12; i++) {
     die.push(new FallingSquare(random(width), -100*i , random(10,15), "blue"));
-    die.push(new FallingCircle(random(width), 0 , random(5,10), "yellow"));
-    die.push(new FallingTriangle(random(width), 0, random(3,5),"purple"));  
+    die.push(new FallingCircle(random(width), 0 , random(10,15), "yellow"));
+    die.push(new FallingTriangle(random(width), 0, random(10,15),"purple"));  
   }
 } 
 
