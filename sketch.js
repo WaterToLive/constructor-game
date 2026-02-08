1521d6e9a9970c7d34cd424a1bb900b94c52f987
WaterToLive <bilalftoure@gmail.com>
2026-02-08 19:49:54 +0000

    squares fall off seperately

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let die= [];
 function setup () {
   createCanvas(windowWidth,windowHeight); 
   for(let i =0; i < 12; i++) {
-    die.push(new FallingSquare(random(width), 0 , random(20,35), "blue"));
+    die.push(new FallingSquare(random(width), -50*i , random(20,35), "blue"));
     die.push(new FallingCircle(random(width), 0 , random(10,30), "yellow"));
     die.push(new FallingTriangle(random(width), 0, random(15,25),"purple"));  
   }
