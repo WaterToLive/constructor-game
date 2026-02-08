d3c2ed8cffe69d781a3106c1830a29f8bfa78ae4
WaterToLive <bilalftoure@gmail.com>
2026-02-08 19:52:34 +0000

    spread apart

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let die= [];
 function setup () {
   createCanvas(windowWidth,windowHeight); 
   for(let i =0; i < 12; i++) {
-    die.push(new FallingSquare(random(width), -50*i , random(10,15), "blue"));
+    die.push(new FallingSquare(random(width), -100*i , random(10,15), "blue"));
     die.push(new FallingCircle(random(width), 0 , random(5,10), "yellow"));
     die.push(new FallingTriangle(random(width), 0, random(3,5),"purple"));  
   }
